8e305813c528afa1784acfe2beb4210c2f904eff
davidmhuitt86 <david.m.huitt@gmail.com>
2026-09-21 15:07:29 -0500

    Move endpoint candidate types into core model

--- include/eke_dx_wire/topology/endpoint_reconstructor.hpp
+++ include/eke_dx_wire/topology/endpoint_reconstructor.hpp
@@ -2,28 +2,7 @@
 
 #include "eke_dx_wire/core/model.hpp"
 
-#include <string>
-#include <vector>
-
 namespace eke::dx::wire {
-
-enum class EndpointKind {
-    GeometricConductorEnd,
-    ComponentTerminal,
-    ConnectorTerminal,
-    Splice,
-    Ground,
-    ExternalConnection,
-    Unresolved
-};
-
-struct EndpointCandidate {
-    std::string id;
-    std::string node_id;
-    EndpointKind kind = EndpointKind::Unresolved;
-    ConfidenceClass confidence = ConfidenceClass::Unresolved;
-    std::vector<std::string> incident_edges;
-};
 
 struct EndpointArtifacts {
     std::vector<EndpointCandidate> candidates;

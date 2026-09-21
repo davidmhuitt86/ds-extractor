26d0b6de2c53400a7ceeb2636383250954cdd50b
davidmhuitt86 <david.m.huitt@gmail.com>
2026-09-21 15:08:30 -0500

    Optimize endpoint incident-edge reconstruction

--- src/topology/endpoint_reconstructor.cpp
+++ src/topology/endpoint_reconstructor.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <sstream>
+#include <unordered_map>
 #include <utility>
 
 namespace eke::dx::wire {
@@ -16,15 +17,22 @@ EndpointArtifacts EndpointReconstructor::reconstruct(
 
     EndpointArtifacts result;
 
+    std::unordered_map<std::string, std::size_t> node_index;
+    node_index.reserve(nodes.size());
+
     std::vector<std::vector<std::string>> incident(nodes.size());
 
+    for (std::size_t i = 0; i < nodes.size(); ++i)
+        node_index.emplace(nodes[i].id, i);
+
     for (const auto& edge : edges) {
-        for (std::size_t i = 0; i < nodes.size(); ++i) {
-            if (nodes[i].id == edge.from_node ||
-                nodes[i].id == edge.to_node) {
-                incident[i].push_back(edge.id);
-            }
-        }
+        const auto from = node_index.find(edge.from_node);
+        if (from != node_index.end())
+            incident[from->second].push_back(edge.id);
+
+        const auto to = node_index.find(edge.to_node);
+        if (to != node_index.end())
+            incident[to->second].push_back(edge.id);
     }
 
     for (std::size_t i = 0; i < nodes.size(); ++i) {

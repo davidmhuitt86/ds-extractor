56345b13d9380aa921d80d79d0a1ec6af474ed33
davidmhuitt86 <david.m.huitt@gmail.com>
2026-09-21 05:42:27 -0500

    Fix malformed SVG exporter string literals

--- src/export/svg_exporter.cpp
+++ src/export/svg_exporter.cpp
@@ -16,26 +16,26 @@ void SvgExporter::export_segments(
         throw std::runtime_error("Unable to create SVG: " + output_path);
     }
 
-    out << "<?xml version="1.0" encoding="UTF-8"?>\n";
-    out << "<svg xmlns="http://www.w3.org/2000/svg" "
-        << "width="" << model.image_width << "" "
-        << "height="" << model.image_height << "" "
-        << "viewBox="0 0 " << model.image_width << " "
-        << model.image_height << "">\n";
+    out << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    out << "<svg xmlns=\"http://www.w3.org/2000/svg\" "
+        << "width=\"" << model.image_width << "\" "
+        << "height=\"" << model.image_height << "\" "
+        << "viewBox=\"0 0 " << model.image_width << " "
+        << model.image_height << "\">\n";
 
-    out << "  <g id="conductor-layer" fill="none" stroke="black" "
-        << "stroke-linecap="round" stroke-linejoin="round">\n";
+    out << "  <g id=\"conductor-layer\" fill=\"none\" stroke=\"black\" "
+        << "stroke-linecap=\"round\" stroke-linejoin=\"round\">\n";
 
     out << std::setprecision(4);
 
     for (const auto& segment : model.conductor_segments) {
-        out << "    <line id="" << segment.id << "" "
-            << "x1="" << segment.geometry.a.x << "" "
-            << "y1="" << segment.geometry.a.y << "" "
-            << "x2="" << segment.geometry.b.x << "" "
-            << "y2="" << segment.geometry.b.y << "" "
-            << "stroke-width="" << std::max(1.0, segment.thickness_px)
-            << "" data-object-type="conductor-segment"/>\n";
+        out << "    <line id=\"" << segment.id << "\" "
+            << "x1=\"" << segment.geometry.a.x << "\" "
+            << "y1=\"" << segment.geometry.a.y << "\" "
+            << "x2=\"" << segment.geometry.b.x << "\" "
+            << "y2=\"" << segment.geometry.b.y << "\" "
+            << "stroke-width=\"" << std::max(1.0, segment.thickness_px)
+            << "\" data-object-type=\"conductor-segment\"/>\n";
     }
 
     out << "  </g>\n";

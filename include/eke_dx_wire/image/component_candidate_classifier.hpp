627915728370cb6daa5404ef17d3c8364a915274
davidmhuitt86 <david.m.huitt@gmail.com>
2026-09-22 05:52:50 -0500

    AP-COMP-001: add component candidate classifier

---
+++ include/eke_dx_wire/image/component_candidate_classifier.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "eke_dx_wire/core/geometry.hpp"
+#include "eke_dx_wire/image/shape_detector.hpp"
+
+#include <string>
+#include <vector>
+
+namespace eke::dx::wire {
+
+enum class ComponentCandidateKind {
+    Enclosure,
+    CircularSymbol,
+    ChassisGround,
+    PrimitiveSymbol,
+    Unknown
+};
+
+struct ComponentCandidate {
+    std::string id;
+    ComponentCandidateKind kind = ComponentCandidateKind::Unknown;
+    std::vector<std::string> shape_ids;
+    BoundingBox bounds {};
+    ConfidenceClass confidence = ConfidenceClass::Unresolved;
+};
+
+class ComponentCandidateClassifier {
+public:
+    [[nodiscard]] std::vector<ComponentCandidate> classify(
+        const ShapeDetectionArtifacts& shapes) const;
+};
+
+} // namespace eke::dx::wire

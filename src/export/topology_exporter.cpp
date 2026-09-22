07912e79c231dd99171ef3d1022c7f7f29939430
davidmhuitt86 <david.m.huitt@gmail.com>
2026-09-22 08:00:00 -0500

    fix topology exporter escaping

--- src/export/topology_exporter.cpp
+++ src/export/topology_exporter.cpp
@@ -14,7 +14,7 @@ std::string json_escape(const std::string& value) {
     for (const char ch : value) {
         switch (ch) {
         case '\\': result += "\\\\"; break;
-        case '"': result += "\\""; break;
+        case '"': result += "\\\""; break;
         case '\n': result += "\\n"; break;
         case '\r': result += "\\r"; break;
         case '\t': result += "\\t"; break;
